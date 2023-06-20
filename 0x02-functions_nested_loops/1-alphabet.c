bb1d747e7e30f8ec9c26be7d794a0b259b8982a9
el-mouf <m.moufki@gmail.com>
2023-06-20 14:08:27 +0000

    correct output and betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - Entry point prints putchar
+ * print_alphabet - prints alphabet in lower case
+ *
  * Return: Always 0
  */
 
@@ -8,7 +9,7 @@ void print_alphabet(void)
 {
 	int i;
 
-	for (i = 97; i < 122 ; i++)
+	for (i = 97; i <= 122 ; i++)
 	{
 		_putchar(i);
 	}
