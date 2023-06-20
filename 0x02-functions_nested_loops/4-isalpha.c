c2663d9342dc99059e6b8d76c78104b55dd8b043
el-mouf <m.moufki@gmail.com>
2023-06-20 15:01:17 +0000

    function checks for lower and upper case alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic characters
+ *
+ * @c: charachter in the ASCII table
+ *
+ * Return: 1 if c is a letter otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
