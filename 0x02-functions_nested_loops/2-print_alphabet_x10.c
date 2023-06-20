00463a2cbd066c09544019b3a1cc327069d60f0c
el-mouf <m.moufki@gmail.com>
2023-06-20 14:21:03 +0000

    print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase ten times
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int i;
+	
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
