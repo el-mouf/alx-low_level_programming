04e1ac669e677c537d7f946927e3920cbfb500fe
el-mouf <m.moufki@gmail.com>
2023-07-03 11:18:10 +0000

    function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a const byte
+ * @n: bytes to be filled
+ * @s: points to the memory
+ * @b: constant byte
+ * Return: a pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+A continuation of the projects on pointers, arrays and strings.
+
+The learning objectives are to understand and be able to explain:
+What are pointers to pointers and how to use them
+What are multidimensional arrays and how to use them
+What are the most common C standard library functions to manipulate strings
