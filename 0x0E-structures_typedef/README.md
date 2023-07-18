372f1264a5de6cbf75bafd8afd666bb3d8b7db11
el-mouf <m.moufki@gmail.com>
2023-07-18 17:45:43 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+This alx project deals with sturctures. 
+
+Goal is to learn:
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new data type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
