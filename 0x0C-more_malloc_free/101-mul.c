9bebdfec3fe96d6e420425c390489065ec057286
el-mouf <m.moufki@gmail.com>
2023-07-25 15:00:31 +0000

    Write a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -42,7 +42,7 @@ int is_digit(const char *s)
 int main(int argc, char * argv[])
 {
 	const char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, num1, num2, *product, a = 0;
 
 	if (argc != 3 || !is_digit(argv[1]) ||!is_digit(argv[2]))
 	{
@@ -53,41 +53,41 @@ int main(int argc, char * argv[])
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	product = malloc(sizeof(int) * len);
 
-	if (!result)
+	if (!product)
 	{
 		return (1);
 	}
 	for (i = 0; i <= len1 + len2; i++)
 	{
-		result[i] = 0;
+		product[i] = 0;
 	}
 	for (len1 = len1 - 1; len1 >= 0; len--)
 	{
-		digit1 = s1[len1] - '0';
+		num1 = s1[len1] - '0';
 		carry = 0;
 		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
+			num2 = s2[len2] - '0';
+			carry += product[len1 + len2 + 1] + (num1 * num2);
+			product[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
 		{
-			result[len1 + len2 + 1] += carry;
+			product[len1 + len2 + 1] += carry;
 		}
 	}
 	for (i = 0; i < len - 1; i++)
 	{
-		if (result[i])
+		if (product[i])
 		{
 			a = 1;
 		}
 		if (a)
 		{
-			_putchar(result[i] + '0');
+			_putchar(product[i] + '0');
 		}
 	}
 	if (!a)
@@ -95,6 +95,6 @@ int main(int argc, char * argv[])
 		_putchar('0');
 	}
 	_putchar('\n');
-	free(result);
+	free(product);
 	return (0);
 }
